de230d0af7b27f4c1074ddf4641cb8363130b6ef
Hunter Hockman <84150506+Anti-Loli@users.noreply.github.com>
2022-10-27 16:29:50 -0400

    Added deck script

---
+++ Card Game/Assets/Deck/Deck.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private int deckLimit = 25;
+    public  Cards[] deck = new Cards[25];
+    private Cards currentCard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawCard()
+    {
+        
+    }
+}

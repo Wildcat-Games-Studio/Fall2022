8ee4c2c04b7963159d1d76cf1830b5e5ee28b83e
Cowman9002 <54774464+Cowman9002@users.noreply.github.com>
2022-10-27 16:11:10 -0400

    Tower aims and shoots

--- Card Game/Assets/Scripts/GenericTower.cs
+++ Card Game/Assets/Scripts/GenericTower.cs
@@ -5,24 +5,40 @@ using UnityEngine;
 public class GenericTower : MonoBehaviour
 {
     [SerializeField]
-    Cards data;
+    public Cards data;
+    [SerializeField]
+    private GameObject _shot;
 
-    CircleCollider2D col;
+    float _next_shot_valid = 0.0f;
+    private CircleCollider2D _col;
+
+    private void Awake()
+    {
+        _col = gameObject.AddComponent<CircleCollider2D>();
+    }
 
     void Start()
     {
-        col = gameObject.AddComponent<CircleCollider2D>();
-        col.radius = data.radius;
-        col.isTrigger = true;
+        _col.radius = data.radius;
+        _col.isTrigger = true;
     }
 
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (Time.time > _next_shot_valid)
+            Shoot(collision);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Shoot(Collider2D target)
     {
-        Debug.Log("enemy in range trigger!");
+        _next_shot_valid = Time.time + 1.0f / data.attackSpeed;
+
+        Vector2 dir = (target.gameObject.transform.position - transform.position).normalized;
+
+        GameObject spawned = Instantiate(_shot);
+        spawned.transform.position = transform.position;
+        spawned.GetComponent<Rigidbody2D>().velocity = dir * 20.0f;
+        spawned.GetComponent<Shot>().owner = this;
+        Destroy(spawned, 10);
     }
 }
